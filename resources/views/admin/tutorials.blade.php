c1c9abfb93536d4edc27ab3ed19302f1e7159d11
Cassio Genehr <cassiogenehrfilho@gmail.com>
2022-08-03 22:02:59 -0300

    listagem tutoriais

--- resources/views/admin/tutorials.blade.php
+++ resources/views/admin/tutorials.blade.php
@@ -8,7 +8,7 @@
     <div class="content">
         <div class="container">
             <div class="table-responsive">
-                <table class="table table-striped">
+                <table class="table table-striped font-color">
                     <thead>
                         <tr>
                             <th scope="col">Youtube ID</th>
